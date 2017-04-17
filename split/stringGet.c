9cebbc762b75e9ab52902ada1444a67d18941682
Paul Humphreys <querycsv@gmail.com>
2017-04-17 23:36:15 +0100

    removed some old files

--- split/stringGet.c
+++ split/stringGet.c
@@ -11,31 +11,4 @@ void stringGet(
   if(str != NULL) {
     *str = (unsigned char *)(strdup((char const *)(field->value)));
   }
-
-  /*
-  long offset = ftell(*(field->source));
-
-  fseek(*(field->source), field->startOffset, SEEK_SET);
-  fflush(*(field->source));
-
-  if (field->isQuoted) {
-    //can't use a shortcut to get the string value, so get it the same way we did the last time
-    getCsvColumn(field->source,ENC_UTF8,str,&(field->length),NULL,NULL, (params & PRM_TRIM) == 0);
-  }
-  else {
-    //can use a shortcut to get the string value
-    reallocMsg((void**)str, field->length+1);
-
-    if(fread(*str, 1, field->length, *(field->source)) != field->length) {
-      fputs("didn't read string properly\n", stderr);
-      exit(EXIT_FAILURE);
-    }
-
-    (*str)[field->length] = '\0';
-  }
-
-  //reset the file offset as we don't know what else the file is being used for
-  fseek(*(field->source), offset, SEEK_SET);
-  fflush(*(field->source));
-  */
 }
