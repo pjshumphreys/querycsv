e3993e50e8e25a6d28609d28c940c24862218f84
Paul Humphreys <querycsv@gmail.com>
2017-04-17 21:07:26 +0100

    charset conversion code is now mostly working. Need to fix collation for codepoints that normalise into multiple ones

--- split/getCsvColumn.c
+++ split/getCsvColumn.c
@@ -16,10 +16,12 @@ int getCsvColumn(
   int byteLength;
 
   int i;
+  int c2;
   long c;
   int state = 0;
 
   char *tempString = NULL;
+  size_t tempSize;
   int canEnd = TRUE;
   int quotePossible = TRUE;
   int exitCode = 0;
@@ -34,6 +36,9 @@ int getCsvColumn(
 
   if(strSize != NULL) {
     *strSize = 0;
+  }
+  else {
+    strSize = &tempSize;
   }
 
   if(value == NULL) {
@@ -98,9 +103,62 @@ int getCsvColumn(
   }
 
   do {
+    if(state != 0) {
+      if(state == 1) {   /* \r detected state. look for \n */
+        state = 0;
+
+        c2 = fgetc(*inputFile);
+
+        if(c2 != '\n') {
+          ungetc(c2, *inputFile);
+        }
+        else {
+          offset++;
+        }
+
+        if(canEnd) {
+          exitCode = 2;
+          break;
+        }
+        else {
+          if (quotedValue != NULL) {
+            *quotedValue = TRUE;
+          }
+          strAppend('\r', value, strSize);
+          strAppend('\n', value, strSize);
+        }
+      }
+      else if(state == 2) {   /* test for double quote literal inside a quoted string state */
+        state = 0;  /* go back to initial state */
+
+        c2 = fgetc(*inputFile);
+
+        switch(c2) {
+          case ' ':  /* ' ' */
+          case '\r':  /* '\r' */
+          case '\n':  /* '\n' */
+          case EOF:
+          case ',': {  /* ',' */
+            canEnd = TRUE;
+            ungetc(c2, *inputFile);
+          } break;
+
+          case '"': {  /* '"' */
+            strAppend('"', value, strSize);
+            offset++;
+          } break;
+
+          default: {
+            strAppend('"', value, strSize);
+            ungetc(c2, *inputFile);
+          } break;
+        }
+      }
+    }
+
     /* get some codepoints from the file, usually only 1 but maybe up to 4 */
     getCodepoints(*inputFile,
-      &codepoints,
+      codepoints,
       &arrLength,
       &byteLength
     );
@@ -112,10 +170,20 @@ int getCsvColumn(
     for (i = 0; i < arrLength && exitCode == 0; i++) {
       c = codepoints[i];
 
-      switch(state) {
-        case 1: {   /* \r detected state. look for \n */
-          state = 0;  /* go back to initial state */
+      switch(c) {
+        case 0x20: { /* ' ' */
+          if(!canEnd) {
+            minSize = &((*value)[*strSize]);
+          }
+          strAppend(' ', value, strSize);
+        } break;
 
+        case 0x0D: { /* '\r' */
+          state = 1;
+          continue;
+        } break;
+
+        case 0x0A: { /* '\n' */
           if(canEnd) {
             exitCode = 2;
             break;
@@ -127,111 +195,50 @@ int getCsvColumn(
             strAppend('\r', value, strSize);
             strAppend('\n', value, strSize);
           }
+        } break;
 
-          if(c == 0x0A /* '\n' */) {  /* consume a \n character after \r */
+        case 0x0: {
+          if (quotedValue != NULL) {
+            *quotedValue = TRUE;
+          }
+        } break;
+
+        case MYEOF: {
+          exitCode = 2;
+        } break;
+
+        case 0x22: {  /* '"' */
+          canEnd = FALSE;
+
+          if (quotedValue != NULL) {
+            *quotedValue = TRUE;
+          }
+
+          if(quotePossible) {
+            *strSize = 0;
+
+            quotePossible = FALSE;
+          }
+          else {
+            state = 2;
             continue;
           }
-        } /* fall thru */
+        } break;
 
-        case 2: { /* test for double quote literal inside a quoted string state */
-          state = 0;  /* go back to initial state */
-
-          switch(c) {
-            case 0x20:  /* ' ' */
-            case 0x0D:  /* '\r' */
-            case 0x0A:  /* '\n' */
-            case MYEOF:
-            case 0x2C: {  /* ',' */
-              canEnd = TRUE;
-            } break;
-
-            case 0x22: {  /* '"' */
-              strAppend('"', value, strSize);
-              continue;
-            } break;
-
-            default: {
-              strAppend('"', value, strSize);
-            } break;
+        case 0x2C: {  /* ',' */
+          if(canEnd) {
+            exitCode = 1;
+            break;
           }
         } /* fall thru */
 
-        default: {   /* initial state */
-          switch(c) {
-            case 0x20: { /* ' ' */
-              if(!canEnd) {
-                minSize = &((*value)[*strSize]);
-              }
-              strAppend(' ', value, strSize);
-            } break;
-
-            case 0x0D: { /* '\r' */
-              state = 1;
-              continue;
-            } break;
-
-            case 0x0A: { /* '\n' */
-              if(canEnd) {
-                exitCode = 2;
-                break;
-              }
-              else {
-                if (quotedValue != NULL) {
-                  *quotedValue = TRUE;
-                }
-                strAppend('\r', value, strSize);
-                strAppend('\n', value, strSize);
-              }
-            } break;
-
-            case 0x0: {
-              if (quotedValue != NULL) {
-                *quotedValue = TRUE;
-              }
-            } break;
-
-            case MYEOF: {
-              exitCode = 2;
-            } break;
-
-            case 0x22: {  /* '"' */
-              canEnd = FALSE;
-
-              if (quotedValue != NULL) {
-                *quotedValue = TRUE;
-              }
-
-              if(quotePossible) {
-                if(strSize != NULL) {
-                  *strSize = 0;
-                }
-
-                quotePossible = FALSE;
-              }
-              else {
-                state = 2;
-                continue;
-              }
-            } break;
-
-            case 0x2C: {  /* ',' */
-              if(canEnd) {
-                exitCode = 1;
-                break;
-              }
-            } /* fall thru */
-
-            default: {
-              if(doTrim && quotePossible) {
-                if(strSize != NULL) {
-                  *strSize = 0;
-                }
-              }
-
-              quotePossible = FALSE;
-              strSize = strAppendUTF8(c, value, *strSize);
-            } break;
+        default: {
+          if(doTrim && quotePossible) {
+            *strSize = 0;
           }
+
+          quotePossible = FALSE;
+          *strSize = strAppendUTF8(c, value, *strSize);
         } break;
       }
     }
@@ -246,6 +253,8 @@ int getCsvColumn(
   if(strSize != NULL) {
     (*strSize)--;
   }
+
+  offset--;
 
   if(startPosition != NULL) {
     *startPosition = offset;
