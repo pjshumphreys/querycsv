f47cba4fe38bad28e36f7b6c6178073624516f98
Paul Humphreys <querycsv@gmail.com>
2017-06-30 16:37:51 +0100

    better charset names list checker

--- split/parse_encoding.c
+++ split/parse_encoding.c
@@ -1,40 +1,114 @@
 #include "querycsv.h"
 
+int mystrnicmp(const char *str1, const char *str2, size_t n) {
+  int i, retval = 0;
+
+  if(n) {
+    for(i = 0; i < n; ++i) {
+      retval = tolower(*str1++) - tolower(*str2++);
+
+      if(retval) {
+        break;
+      }
+
+      if(*str1 && *str2) {
+        continue;
+      }
+      else {
+        break;
+      }
+    }
+  }
+
+  return retval;
+}
+
 int parse_encoding(
     struct qryData *queryData,
     char *encoding
   ) {
+  char *matcher;
+    
   MAC_YIELD
 
-  if(stricmp("cp1252", encoding) == 0) { /* always windows 1252 */
-    return ENC_CP1252;
+  if(mystrnicmp("utf", encoding, 3) == 0) {
+    matcher = encoding +3;
+    encoding += 3;
+
+    if(mystrnicmp("-", matcher, 1) == 0) {
+      matcher += 1;
+    }
+
+    if(strcmp("8", matcher) == 0) {
+      /* utf-8, unless a BOM is found. Overlong bytes
+      are always converted using windows 1252 */
+      return ENC_UTF8;
+    }
+
+    if(
+        strcmp("16", matcher) == 0 ||
+        stricmp("16le", matcher) == 0
+    ) {
+      return ENC_UTF16LE;
+    }
+
+    if(stricmp("16be", matcher) == 0) {
+      return ENC_UTF16BE;
+    }
+
+    if(
+        strcmp("32", matcher) == 0 ||
+        stricmp("32le", matcher) == 0
+    ) {
+      return ENC_UTF32LE;
+    }
+
+    if(stricmp("32be", matcher) == 0) {
+      return ENC_UTF32BE;
+    }
   }
 
-  if(stricmp("cp437", encoding) == 0) {  /* always CP437 */
-    return ENC_CP437;
+  else if(mystrnicmp("cp", encoding, 2) == 0) {
+    matcher = encoding + 2;
+
+    if(strcmp("1252", matcher) == 0) { /* always windows 1252 */
+      return ENC_CP1252;
+    }
+
+    if(strcmp("437", matcher) == 0) {  /* always CP437 */
+      return ENC_CP437;
+    }
+    
+    if(strcmp("850", matcher) == 0) {  /* always CP850 */
+      return ENC_CP850;
+    }
+
+    if(strcmp("1047", matcher) == 0) {
+      return ENC_CP1047;
+    }
   }
 
-  if(stricmp("cp850", encoding) == 0) {  /* always CP850 */
-    return ENC_CP850;
-  }
-
-  if(
-      stricmp("utf8", encoding) == 0 ||
-      stricmp("unicode", encoding) == 0) {  /* utf-8, unless a BOM is found,
-        overlong bytes are always converted using windows 1252 */
-    return ENC_UTF8;
-  }
-
-  if(stricmp("petscii", encoding) == 0) {
-    return ENC_PETSCII;
-  }
-
-  if(stricmp("mac", encoding) == 0) {
+  else if(
+      stricmp("mac", encoding) == 0 ||
+      stricmp("macroman", encoding) == 0
+  ) {
     return ENC_MAC;
   }
 
-  if(stricmp("cp1047", encoding) == 0) {
-    return ENC_CP1047;
+  else if(stricmp("oem", encoding) == 0) {
+    return ENC_CP437;
+  }
+
+  else if(stricmp("ansi", encoding) == 0) {
+    return ENC_CP1252;
+  }
+
+  else if(stricmp("unicode", encoding) == 0) {
+    return ENC_UTF8;
+  }
+
+  else if(stricmp("petscii", encoding) == 0) {
+    return ENC_PETSCII;
   }
 
   fprintf(stderr, "unsupported encoding %s\n", encoding);
