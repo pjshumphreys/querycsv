f26eeb0dc9a308566b2b1072c84a91f254764203
Paul Humphreys <pjshumphreys@blueyonder.co.uk>
2018-07-02 01:16:42 +0100

    added missing files

---
+++ split/getCodepointsBBC.c
@@ -0,0 +1,29 @@
+void getCodepointsBBC(
+    FILE *stream,
+    long *codepoints,
+    int *arrLength,
+    int *byteLength
+) {
+  int c;
+
+  MAC_YIELD
+
+  if(stream == NULL) {
+    *arrLength = *byteLength = 0;
+    return;
+  }
+
+  *arrLength = *byteLength = 1;
+
+  if((c = fgetc(stream)) == EOF) {
+    codepoints[0] = MYEOF;
+    return;
+  }
+
+  if(c == 96) {
+    codepoints[0] = 163;
+    return;
+  }
+
+  codepoints[0] = (long)c;
+}
