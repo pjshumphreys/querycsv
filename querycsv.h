87b44cb54d8b90f6013e3517ad9064da61ee03c6
Paul Humphreys <pjshumphreys@blueyonder.co.uk>
2018-07-13 19:41:29 +0100

    make the ability to use softfloat an option for any compiler now

--- querycsv.h
+++ querycsv.h
@@ -103,120 +103,6 @@ but hasn't actually been needed up to now. It's being kept just in case
 it becomes needed */
 #define MAC_YIELD
 
-/* Old versions of Norcroft appear to generate incorrect codes with short integers.
-Just use long ones for that compiler */
-#define QCSV_SHORT short
-
-/* ugly hacks to add floating point emulation to Norcroft version 2 */
-#if defined(__CC_NORCROFT)
-  #define __fastcall__ /* do nothing */
-
-  #if __LIB_VERSION < 300
-    /* doesn't do well with 16 bit data types, so use the 32 bit ones all the time */
-    #define YYTYPE_UINT16 unsigned int
-    #define YYTYPE_INT16 int
-    #undef QCSV_SHORT
-    #define QCSV_SHORT long
-
-    #undef ENC_INPUT
-    #define ENC_INPUT ENC_BBC
-    #undef ENC_OUTPUT
-    #define ENC_OUTPUT ENC_BBC
-    #undef ENC_PRINT
-    #define ENC_PRINT ENC_BBC
-  #else
-    #include <unixlib.h> /*for chdir? */
-
-    void setupRiscOS(int *argc, char ***argv);  /* additional stuff needed at start up */
-
-    #undef ENC_OUTPUT
-    #define ENC_OUTPUT ENC_CP1252
-    #undef ENC_PRINT
-    #define ENC_PRINT ENC_CP1252
-  #endif
-
-  #include "milieu.h"
-  #include "macros.h" /* fudges kinda support for floating point
-  into Norcroft C version 2 by utilising John Hauser's Berkeley SoftFloat package */
-  #include "softfloat.h"
-
-  char* mydtoa(char *s, double n);
-  double mystrtod(const char *str, char **end);
-  extern double fltMinusOne;
-  #define strtod mystrtod /* Norcroft C does have strtod, but using it generates
-  floating point instructions which we don't want as we're using softfloat.
-  Hence this macro that swaps it for our own implementation */
-
-  #define YY_NO_UNISTD_H 1
-  #define HAS_KERNEL_SWI   /* Later versions of Norcroft have vsnprintf,
-  but the early ones don't. Therefore we use the fprintf approach. */
-  #include <kernel.h> /* for _kernel_osbyte function (used by d_sprintf) */
-
-  int stricmp(const char *str1, const char *str2);
-
-  /* manually define the errno values that lexer uses as errno.h doesn't
-     exist in version 2 but lexer.c uses these defines */
-  #define EINTR  4    /* Interrupted system call */
-  #define ENOMEM 12   /* Cannot allocate memory */
-  #define EINVAL 22   /* Invalid value */
-
-  /* These aren't in norcroft version 2's stdlib.h */
-  #define EXIT_FAILURE 1
-  #define EXIT_SUCCESS 0
-
-  #define in_word_set_a in_word_set_ai
-  #define in_word_set_b in_word_set_bi
-  #define in_word_set_c in_word_set_ci
-
-/* ugly hacks to raise the game of cc65 */
-#elif defined(__CC65__)
-  #define YY_NO_UNISTD_H 1
-  #define HAS_VSNPRINTF /* although cc65 doesn't have floating point,
-  at least it has vsnprintf*/
-
-  #include <conio.h> /* for cgetc */
-  #include "floatlib/float.h" /* fudges kinda support for floating point
-  into cc65 by utilising functionality in the c64 basic rom */
-  #define ftostr(_d,_a) { \
-    reallocMsg((void**)_d, 33); \
-    _ftostr(*(_d), (_a)); \
-    reallocMsg((void**)_d, strlen(*(_d)) + 1); \
-    } /* the _ftostr function in cc65-floatlib seems to output at
-    most 32 characters */
-  #define fneg(_d) _fneg(_d)
-  double strtod(const char* str, char** endptr);  /* cc65 doesn't
-  have strtod (as it doesn't have built in floating point number support).
-  We supply our own implementation that provides the same semantics but
-  uses cc65-floatlib */
-
-  #undef ENC_INPUT
-  #undef ENC_OUTPUT
-  #undef ENC_PRINT
-  #define ENC_INPUT ENC_PETSCII
-  #define ENC_OUTPUT ENC_PETSCII
-  #define ENC_PRINT ENC_PETSCII
-#else
-  #define __fastcall__ /* do nothing */
-
-  /* duplicates of the macros in cc65-floatlib that just use the
-  native floating point support */
-  #define fadd(_f,_a) ((_f)+(_a))
-  #define fsub(_f,_a) ((_f)-(_a))
-  #define fmul(_f,_a) ((_f)*(_a))
-  #define fdiv(_f,_a) ((_f)/(_a))
-  #define fcmp(_d,_s) ((_d)!=(_s))
-  #define ctof(_s) ((double)(_s))
-  #define fneg(_f) ((_f)*(-1))
-  #define ftostr(_f,_a) (_a) != (_a) ? \
-    d_sprintf((_f), "NaN") : \
-    d_sprintf((_f), "%g", (_a)) /* d_sprintf knows how to
-    convert doubles to strings */
-
-  #define in_word_set_a in_word_set_ai
-  #define in_word_set_b in_word_set_bi
-  #define in_word_set_c in_word_set_ci
-#endif
-
 #if defined(__unix__) || defined(__LINUX__)
   #if defined(EMSCRIPTEN)
     #define main realmain
@@ -293,6 +179,62 @@ Just use long ones for that compiler */
   #endif
 #endif
 
+/* Old versions of Norcroft appear to generate incorrect codes with short integers.
+Just use long ones for that compiler */
+#define QCSV_SHORT short
+
+#ifdef __CC_NORCROFT
+  #define __fastcall__ /* do nothing */
+
+  #define YY_NO_UNISTD_H 1
+  #define HAS_KERNEL_SWI   /* Later versions of Norcroft have vsnprintf,
+  but the early ones don't. Therefore we use the fprintf approach. */
+  #include <kernel.h> /* for _kernel_osbyte function (used by d_sprintf) */
+
+  int stricmp(const char *str1, const char *str2);
+
+  /* manually define the errno values that lexer uses as errno.h doesn't
+     exist in version 2 but lexer.c uses these defines */
+  #define EINTR  4    /* Interrupted system call */
+  #define ENOMEM 12   /* Cannot allocate memory */
+  #define EINVAL 22   /* Invalid value */
+
+  /* These aren't in norcroft version 2's stdlib.h */
+  #define EXIT_FAILURE 1
+  #define EXIT_SUCCESS 0
+
+  #define in_word_set_a in_word_set_ai
+  #define in_word_set_b in_word_set_bi
+  #define in_word_set_c in_word_set_ci
+
+  #define SOFTFLOAT /* Use John Hauser's softfloat package rather
+  than the compiler's built in floating point implementation */
+
+  #if __LIB_VERSION < 300
+    /* doesn't do well with 16 bit data types, so use the 32 bit ones all the time */
+    #define YYTYPE_UINT16 unsigned int
+    #define YYTYPE_INT16 int
+    #undef QCSV_SHORT
+    #define QCSV_SHORT long
+
+    #undef ENC_INPUT
+    #define ENC_INPUT ENC_BBC
+    #undef ENC_OUTPUT
+    #define ENC_OUTPUT ENC_BBC
+    #undef ENC_PRINT
+    #define ENC_PRINT ENC_BBC
+  #else
+    #include <unixlib.h> /*for chdir? */
+
+    void setupRiscOS(int *argc, char ***argv);  /* additional stuff needed at start up */
+
+    #undef ENC_OUTPUT
+    #define ENC_OUTPUT ENC_CP1252
+    #undef ENC_PRINT
+    #define ENC_PRINT ENC_CP1252
+  #endif
+#endif
+
 #ifdef __VBCC__
   #define HAS_VSNPRINTF
 #endif
@@ -325,6 +267,67 @@ Just use long ones for that compiler */
   #define ENC_OUTPUT ENC_ATARIST
   #undef ENC_PRINT
   #define ENC_PRINT ENC_ATARIST
+#endif
+
+/* ugly hacks to raise the game of cc65 */
+#ifdef __CC65__
+  #define YY_NO_UNISTD_H 1
+  #define HAS_VSNPRINTF /* although cc65 doesn't have floating point,
+  at least it has vsnprintf*/
+
+  #include <conio.h> /* for cgetc */
+  #include "floatlib/float.h" /* fudges kinda support for floating point
+  into cc65 by utilising functionality in the c64 basic rom */
+  #define ftostr(_d,_a) { \
+    reallocMsg((void**)_d, 33); \
+    _ftostr(*(_d), (_a)); \
+    reallocMsg((void**)_d, strlen(*(_d)) + 1); \
+    } /* the _ftostr function in cc65-floatlib seems to output at
+    most 32 characters */
+  #define fneg(_d) _fneg(_d)
+  double strtod(const char* str, char** endptr);  /* cc65 doesn't
+  have strtod (as it doesn't have built in floating point number support).
+  We supply our own implementation that provides the same semantics but
+  uses cc65-floatlib */
+
+  #undef ENC_INPUT
+  #undef ENC_OUTPUT
+  #undef ENC_PRINT
+  #define ENC_INPUT ENC_PETSCII
+  #define ENC_OUTPUT ENC_PETSCII
+  #define ENC_PRINT ENC_PETSCII
+#else
+  #define __fastcall__ /* do nothing */
+
+  #ifdef SOFTFLOAT
+    #include "milieu.h"
+    #include "macros.h"
+    #include "softfloat.h"
+
+    char* mydtoa(char *s, double n);
+    double mystrtod(const char *str, char **end);
+    extern double fltMinusOne;
+
+    #define strtod mystrtod /* swap strtod for our own implementation */
+  #else
+    /* duplicates of the macros in cc65-floatlib that just use the
+    native floating point support */
+    #define fadd(_f,_a) ((_f)+(_a))
+    #define fsub(_f,_a) ((_f)-(_a))
+    #define fmul(_f,_a) ((_f)*(_a))
+    #define fdiv(_f,_a) ((_f)/(_a))
+    #define fcmp(_d,_s) ((_d)!=(_s))
+    #define ctof(_s) ((double)(_s))
+    #define fneg(_f) ((_f)*(-1))
+    #define ftostr(_f,_a) (_a) != (_a) ? \
+      d_sprintf((_f), "NaN") : \
+      d_sprintf((_f), "%g", (_a)) /* d_sprintf knows how to
+      convert doubles to strings */
+  #endif
+
+  #define in_word_set_a in_word_set_ai
+  #define in_word_set_b in_word_set_bi
+  #define in_word_set_c in_word_set_ci
 #endif
 
 /* structures */
