491a90ce65fe4d16aac3ad8c8dc023757892fbe4
Paul Humphreys <pjshumphreys@blueyonder.co.uk>
2019-05-09 11:54:24 +0100

    added memory maps for constrained platforms;added the begginnings of a cpm/msxdos2 port

---
+++ env/cpm/libc.c
@@ -0,0 +1,68 @@
+/* fake program to get the necessary libc functions into 1 memory page */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+unsigned char heap[1];
+
+char* c = "%d";
+
+int ferror(FILE * stream) {
+  return 0;
+}
+
+void foobar(char *string, char* format, ...) {
+  va_list args;
+  va_list args2;
+
+  FILE* test;
+  int num;
+
+  string = malloc(1);
+  fgets(string, 1, stdin);
+  free(string);
+  string = calloc(1,3);
+  string = realloc(string,5);
+  strcpy(string, c);
+  fgets(string,1,stdin);
+  num = strcmp(c, string);
+  num = strncmp(c, string, 3);
+  num = strlen(string);
+  string = strstr(string, c);
+
+  memset(string, 0, 4);
+  strcat(string, c);
+  strncat(string, c, 3);
+  memcpy(string+1, string, 2);
+  memmove(string+1, string, 2);
+
+  test = fopen(c,"rb");
+  num = ferror(test);
+  clearerr(test);
+  num = fclose(test);
+  fread(string, 2, 2, stdin);
+  num = fgetc(stdin);
+  ungetc(num, stdin);
+  num = feof(stdin);
+  fwrite(string, 1, 1, stdout);
+  num = fgetc(stdin);
+  fputc(num, stderr);
+  fflush(stdout);
+  sprintf(string, c, num);
+
+  va_start(args, format);
+  vsprintf(string, format, args);
+  va_end(args);
+
+  va_start(args2, format);
+  vsnprintf(string, 2, format, args2);
+  va_end(args2);
+
+  free(string);
+}
+
+int main(void) {
+  foobar(NULL, c);
+  return 0;
+}
