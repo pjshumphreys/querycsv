8f6d646cca670bfc52a6a04a55c1576bbd1684a4
Paul Humphreys <querycsv@gmail.com>
2021-02-18 00:15:05 +0000

    Updated test suite runner so the current directory doesn't need to be changed

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,5 +1,12 @@
-for file in ./*.qry
-do
-  echo "$file"
-  ../env/posix/querycsv "$file" > /dev/null
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+for i in $(find "$DIR" -name \*.qry | sort -V); do
+  f="$(basename -- $i)"
+  echo "$f"
+ $DIR/../querycsv "$i" > /dev/null
 done
